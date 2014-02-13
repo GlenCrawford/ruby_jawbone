2b8560bf4835d76b2806926f03be97c33e6548ba
Glen Crawford <glencraw4d@gmail.com>
2014-02-14 00:16:55 +1100

    Build parser for handling Jawbone timestamps, represented by an offset before (negative number) or after (positive number) midnight.

--- lib/ruby_jawbone/file_readers/jawbone.rb
+++ lib/ruby_jawbone/file_readers/jawbone.rb
@@ -19,8 +19,8 @@ module RubyJawbone
               :date => date,
               :steps => row[12].to_i, # Total steps taken.
               :distance => row[8].to_i, # Total distance travelled (in meters).
-              :time_active => row[6].to_i, # Total active time (in seconds).
-              :time_inactive => row[9].to_i, # Total inactive time (in seconds).
+              :total_time_active => row[6].to_i, # Total active time (in seconds).
+              :total_time_inactive => row[9].to_i, # Total inactive time (in seconds).
               :longest_time_active => row[10].to_i, # Longest time active (in seconds).
               :longest_time_inactive => row[11].to_i, # Longest time idle (in seconds).
               :calories_burned_through_activity => row[7].to_i, # Number of calories burned directly from activity.
@@ -30,19 +30,43 @@ module RubyJawbone
             results[:sleep] << {
               :date => date,
 
-              # TODO (these three): Seconds to/from midnight. If negative, subtract from midnight. If positive, add to midnight.
               :s_bedtime => row[20], # Time entered sleep mode.
               :s_asleep_time => row[16], # Time fell asleep.
               :s_awake_time => row[18], # Final time woken up.
 
               :total_time_asleep => row[23].to_i, # Total time asleep during sleep period (in seconds).
-              :time_in_light_sleep => row[24].to_i, # Time in light sleep during sleep period (in seconds).
-              :time_in_deep_sleep => row[22].to_i, # Time in deep sleep during sleep period (in seconds).
-              :time_awake => row[17].to_i, # Time awake during sleep period (in seconds).
-              :times_awake => row[19].to_i # Times woken up during sleep period.
+              :total_time_in_light_sleep => row[24].to_i, # Time in light sleep during sleep period (in seconds).
+              :total_time_in_deep_sleep => row[22].to_i, # Time in deep sleep during sleep period (in seconds).
+              :total_time_awake => row[17].to_i, # Time awake during sleep period (in seconds).
+              :times_woken_up => row[19].to_i # Times woken up during sleep period.
             }
           end
         end
+      end
+
+      protected
+
+      # The Jawbone data stores data in an offset before or after midnight (in seconds):
+      # * If negative, before midnight.
+      # * If positive, after midnight.
+      #
+      # Examples:
+      # * Timestamp -3648 for period 21/01/2014 = 10:59pm on 20/01/2014 (before midnight the day before).
+      # * Timestamp 29052 for period 21/01/2014 = 8:04am on 21/01/2014.
+      def parse_time(timestamp, date)
+        minutes = timestamp / 60.to_f
+
+        hour = (minutes / 60).to_i.abs
+        minutes = (minutes % 60).to_i
+
+        # If the time is before midnight, subtract a day and set the hour to the number
+        # of hours before midnight.
+        if timestamp < 0
+          date = date - 1
+          hour = 23 - hour
+        end
+
+        Time.local(date.year, date.month, date.day, hour, minutes)
       end
     end
   end
--- spec/file_readers/jawbone_spec.rb
+++ spec/file_readers/jawbone_spec.rb
@@ -1,5 +1,35 @@
 require 'spec_helper'
 
 describe RubyJawbone::FileReaders::Jawbone do
-  #
+  let(:file) { double "File" }
+  let(:file_reader) { RubyJawbone::FileReaders::Jawbone.new(file) }
+
+  describe "#parse_time" do
+    it "returns the correct time for a negative timestamp (before midnight)" do
+      timestamp = -3648
+      date = Date.new(2014, 1, 21)
+
+      parsed_time = file_reader.send(:parse_time, timestamp, date)
+
+      expect(parsed_time).to eq Time.local(2014, 1, 20, 22, 59)
+    end
+
+    it "returns the correct time for a positive timestamp (after midnight)" do
+      timestamp = 5279
+      date = Date.new(2014, 2, 7)
+
+      parsed_time = file_reader.send(:parse_time, timestamp, date)
+
+      expect(parsed_time).to eq Time.local(2014, 2, 7, 1, 27)
+    end
+
+    it "returns the correct time for zero timestamp (midnight)" do
+      timestamp = 0
+      date = Date.new(2014, 2, 7)
+
+      parsed_time = file_reader.send(:parse_time, timestamp, date)
+
+      expect(parsed_time).to eq Time.local(2014, 2, 7, 0, 0)
+    end
+  end
 end
