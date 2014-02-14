004bf67ff116fe7121951320287cb857afadc9e2
Glen Crawford <glencraw4d@gmail.com>
2014-02-14 23:43:25 +1100

    Add small tests to cover a file reader being initialized with a file.

--- spec/file_readers/base_spec.rb
+++ spec/file_readers/base_spec.rb
@@ -1,5 +1,13 @@
 require 'spec_helper'
 
 describe RubyJawbone::FileReaders::Base do
-  #
+  let(:file) { double "File" }
+
+  describe "#initialize" do
+    it "receives and sets the file" do
+      file_reader = RubyJawbone::FileReaders::Base.new(file)
+
+      expect(file_reader.file).to eq file
+    end
+  end
 end
