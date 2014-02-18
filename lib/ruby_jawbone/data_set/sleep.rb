409d194a6d7eea1d18b52a4d8804880e44e65c95
Glen Crawford <glencraw4d@gmail.com>
2014-02-19 08:51:31 +1100

    Fill in the Sleep data set class to initialize with specified values.

--- lib/ruby_jawbone/data_set/sleep.rb
+++ lib/ruby_jawbone/data_set/sleep.rb
@@ -3,7 +3,20 @@ require 'ruby_jawbone/data_set/base'
 module RubyJawbone
   module DataSet
     class Sleep < Base
-      #
+      attr_reader :date, :bed_time, :asleep_time, :awake_time, :total_time_asleep, :total_time_awake,
+                  :total_time_in_light_sleep, :total_time_in_deep_sleep, :times_woken_up
+
+      def initialize(date, bed_time, asleep_time, awake_time, total_time_asleep, total_time_awake, total_time_in_light_sleep, total_time_in_deep_sleep, times_woken_up)
+        @date = date
+        @bed_time = bed_time
+        @asleep_time = asleep_time
+        @awake_time = awake_time
+        @total_time_asleep = total_time_asleep
+        @total_time_awake = total_time_awake
+        @total_time_in_light_sleep = total_time_in_light_sleep
+        @total_time_in_deep_sleep = total_time_in_deep_sleep
+        @times_woken_up = times_woken_up
+      end
     end
   end
 end
