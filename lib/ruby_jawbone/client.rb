e3ddbb6d1676c78a2ecc2dd81eefc8eeac22b61d
Glen Crawford <glencraw4d@gmail.com>
2014-02-21 17:29:36 +1100

    Initialize a client and first cut at processing a data file.

--- lib/ruby_jawbone/client.rb
+++ lib/ruby_jawbone/client.rb
@@ -5,6 +5,26 @@ require 'ruby_jawbone/data_set/sleep'
 
 module RubyJawbone
   class Client
-    #
+    attr_reader :activity, :sleep
+
+    def initialize
+      @activity = []
+      @sleep = []
+    end
+
+    def process_file(file)
+      parsed_data = FileReaders::Jawbone.new(file).parse_file
+
+      activity = parsed_data[:activity].map do |parsed_activity|
+        DataSet::Activity.new(*parsed_activity.values)
+      end
+
+      sleep = parsed_data[:sleep].map do |parsed_sleep|
+        DataSet::Sleep.new(*parsed_sleep.values)
+      end
+
+      @activity += activity
+      @sleep += sleep
+    end
   end
 end
